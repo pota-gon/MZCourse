72c0b3d7adbc5129a509d51edfc8df4bc78e0b37
pota-gon <19222929+pota-gon@users.noreply.github.com>
2021-08-15 23:04:22 +0900

    『第1章 プラグイン開発の準備』を先行公開

---
+++ 1/1-5-1/Template.js
@@ -0,0 +1,10 @@
+/*:
+ * @target MZ
+ * @plugindesc テンプレート
+ * @author ユーザー名
+ * @help テンプレートです!
+ * @url https://raw.githubusercontent.com/pota-gon/MZCourse/main/1/1-5-1/Template.js
+ */
+(() => {
+    'use strict';
+})();
