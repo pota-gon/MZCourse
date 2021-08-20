f37a32fe34340d73036f05d573ca16d691c71bcb
pota-gon <19222929+pota-gon@users.noreply.github.com>
2021-08-21 00:46:48 +0900

    @url がおかしくなっていた問題修正

--- 1/1-5/Template.js
+++ 1/1-5/Template.js
@@ -3,7 +3,7 @@
  * @plugindesc テンプレート
  * @author ユーザー名
  * @help テンプレートです!
- * @url https://raw.githubusercontent.com/pota-gon/MZCourse/main/1/1-5-1/Template.js
+ * @url https://raw.githubusercontent.com/pota-gon/MZCourse/main/1/1-5/Template.js
  */
 (() => {
     'use strict';
